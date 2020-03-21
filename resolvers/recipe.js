d83eefee8aeaa894edf94eb6e083d894b0342808
kevin tan <kevintan203@gmail.com>
2020-03-21 20:17:20 +0700

    add search feature

--- resolvers/recipe.js
+++ resolvers/recipe.js
@@ -1,9 +1,15 @@
-const {getRecipes} = require('../querys/food')
+const {getRecipes, searchRecipes} = require('../querys/food')
 
 const resolvers = {
   Query: {
     async getRecipes(_,{ ingredients },context,info){
       const response = await getRecipes(ingredients)
+      console.log(response)
+      return response
+    },
+    async searchRecipes(_,{ input },context,info){
+      console.log(input,"<<")
+      const response = await searchRecipes(input)
       console.log(response)
       return response
     },
