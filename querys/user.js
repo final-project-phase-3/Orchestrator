99a3d2874e4b74abcbe1b4fe06a539c82187243d
Amalya Adyanissa <amalya.adyanissa@example.com>
2020-03-24 20:02:30 +0700

    tambah register

---
+++ querys/user.js
@@ -0,0 +1,24 @@
+const axios = require("axios");
+var Redis = require("ioredis");
+const redis = new Redis();
+const url = "http://localhost:3001/";
+const { ApolloError } = require("apollo-server");
+
+async function register(data) {
+  try {
+    const response = await axios({
+      method: "POST",
+      url: `${url}user/register`,
+      data: {
+        username: data.username,
+        password: data.password,
+        email: data.email
+      }
+    });
+    return response.data.userRegistered;
+  } catch (error) {
+    return new ApolloError(error.response.data.msg, error.response.status);
+  }
+}
+
+module.exports = { register };
--- resolvers/user.js
+++ resolvers/user.js
@@ -1,14 +1,25 @@
-const {getUser} = require('../querys/food')
+const { getUser } = require("../querys/food");
+const { register } = require("../querys/user");
 
 const resolvers = {
   Query: {
-    async getUser(_,input,{token},info){
-      const response = await getUser(token)
-      console.log(response)
-      return response
-    },
+    async getUser(_, input, { token }, info) {
+      const response = await getUser(token);
+      console.log(response);
+      return response;
+    }
+  },
+  Mutation: {
+    async register(_, args, context) {
+      const data = {
+        username: args.username,
+        email: args.email,
+        password: args.password
+      };
+      const response = await register(data);
+      return response;
+    }
   }
 };
 
-
-module.exports = resolvers
+module.exports = resolvers;
